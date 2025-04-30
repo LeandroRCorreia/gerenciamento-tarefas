792c99f96cb2b34d9068e7a0cb082ef85f811644
LeandroRCorreia <leandrohamlet@gmail.com>
2025-04-30 13:06:30 -0300

    feat: adição de logs descritivos em tarefaServiceImpl e algumas refatorações

--- src/main/java/org/perenity/gerenciamentotarefas/business/tarefa/service/impl/TarefaServiceImpl.java
+++ src/main/java/org/perenity/gerenciamentotarefas/business/tarefa/service/impl/TarefaServiceImpl.java
@@ -22,26 +22,26 @@ public class TarefaServiceImpl implements TarefaService {
 
     @Override
     public Collection<Tarefa> listarTarefasPendentes() {
+        log.info("[Tarefa-Api][listarTarefasPendentes] Listando tarefas pendentes");
         return tarefaGateway.listarTarefasPendentes();
     }
 
     @Override
     public Tarefa cadastrarTarefa(final Tarefa tarefa) {
+        log.info("[Tarefa-Api][cadastrarTarefa] Cadastrando nova tarefa: {}", tarefa);
         return tarefaGateway.cadastrar(tarefa);
     }
 
     @Override
     public void alocarTarefaPessoa(final Long id, final Tarefa tarefa) {
-        final Tarefa tarefaEncontrada = tarefaGateway
-                .buscarTarefa(id)
-                .orElseThrow(() -> new NotFoundException("Tarefa não encontrada com ID: " + id));
+        log.info("[Tarefa-Api][alocarTarefaPessoa] Iniciando alocação da tarefa com ID {} para a pessoa com ID {}", id, tarefa.getPessoaId());
 
-        final Pessoa pessoaEncontrada = pessoaGateway
-                .buscarPessoa(tarefa.getPessoaId())
-                .orElseThrow(() -> new NotFoundException("Pessoa não encontrada com ID: " + tarefa.getPessoaId()));
+        final Tarefa tarefaEncontrada = buscarTarefaOuFalhar(id);
+        final Pessoa pessoaEncontrada = buscarPessoaOuFalhar(tarefa.getPessoaId());
 
-        //TODO: Alinhamento do campo departamento
         if (!tarefaEncontrada.getDepartamento().equalsIgnoreCase(pessoaEncontrada.getDepartamento())) {
+            log.warn("[Tarefa-Api][alocarTarefaPessoa] Departamento da tarefa ({}) é diferente do departamento da pessoa ({})",
+                    tarefaEncontrada.getDepartamento(), pessoaEncontrada.getDepartamento());
             throw new IllegalArgumentException("Departamento da tarefa e da pessoa não são compatíveis");
         }
 
@@ -51,17 +51,15 @@ public class TarefaServiceImpl implements TarefaService {
                 .build();
 
         tarefaGateway.atualizar(id, tarefaComPessoaAlocada);
+        log.info("[Tarefa-Api][alocarTarefaPessoa] Tarefa com ID {} alocada para a pessoa com ID {}", id, tarefa.getPessoaId());
     }
 
     @Override
     public void finalizarTarefa(final Long id, final Tarefa tarefa) {
-        final Tarefa tarefaEncontrada = tarefaGateway
-                .buscarTarefa(id)
-                .orElseThrow(() -> new NotFoundException("Tarefa não encontrada com ID: " + id));
+        log.info("[Tarefa-Api][finalizarTarefa] Finalizando tarefa com ID {} para pessoa ID {}", id, tarefa.getPessoaId());
 
-        final Pessoa pessoaEncontrada = pessoaGateway
-                .buscarPessoa(tarefa.getPessoaId())
-                .orElseThrow(() -> new NotFoundException("Pessoa não encontrada ou não alocada em tarefa com ID: " + tarefa.getPessoaId()));
+        final Tarefa tarefaEncontrada = buscarTarefaOuFalhar(id);
+        buscarPessoaOuFalhar(tarefa.getPessoaId());
 
         final Tarefa tarefaFinalizada = tarefaEncontrada.toBuilder()
                 .finalizado(Boolean.TRUE)
@@ -69,5 +67,25 @@ public class TarefaServiceImpl implements TarefaService {
                 .build();
 
         tarefaGateway.atualizar(id, tarefaFinalizada);
+        log.info("[Tarefa-Api][finalizarTarefa] Tarefa com ID {} finalizada com sucesso. Duração registrada: {} horas", id, tarefa.getDuracaoHoras());
     }
+
+    private Tarefa buscarTarefaOuFalhar(Long tarefaId) {
+        return tarefaGateway
+                .buscarTarefa(tarefaId)
+                .orElseThrow(() -> {
+                    log.warn("[Tarefa-Api][buscarTarefaOuFalhar] Tarefa com ID {} não encontrada", tarefaId);
+                    return new NotFoundException("Tarefa não encontrada com ID: " + tarefaId);
+                });
+    }
+
+    private Pessoa buscarPessoaOuFalhar(Long pessoaId) {
+        return pessoaGateway
+                .buscarPessoa(pessoaId)
+                .orElseThrow(() -> {
+                    log.warn("[Tarefa-Api][buscarPessoaOuFalhar] Pessoa com ID {} não encontrada", pessoaId);
+                    return new NotFoundException("Pessoa não encontrada com ID: " + pessoaId);
+                });
+    }
+
 }
