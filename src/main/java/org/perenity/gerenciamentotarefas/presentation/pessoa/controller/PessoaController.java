b07b987c18096b09a473d46f6faaa9bb52a9afe8
LeandroRCorreia <leandrohamlet@gmail.com>
2025-05-18 12:32:46 -0300

    feat: padronização de saida da api

--- src/main/java/org/perenity/gerenciamentotarefas/presentation/pessoa/controller/PessoaController.java
+++ src/main/java/org/perenity/gerenciamentotarefas/presentation/pessoa/controller/PessoaController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.perenity.gerenciamentotarefas.business.pessoa.service.PessoaService;
+import org.perenity.gerenciamentotarefas.infra.ResponseDto;
 import org.perenity.gerenciamentotarefas.presentation.pessoa.dto.request.RequestAtualizarPessoa;
 import org.perenity.gerenciamentotarefas.presentation.pessoa.dto.request.RequestCadastrarPessoa;
 import org.perenity.gerenciamentotarefas.presentation.pessoa.dto.response.ResponseListarPessoasNomePeriodo;
@@ -28,56 +29,48 @@ public class PessoaController {
     private final PessoaDtoMapper pessoaDtoMapper;
 
     @GetMapping("/gastos")
-    public ResponseEntity<Collection<ResponseListarPessoasNomePeriodo>> listarPessoasPorNomePeriodo(
+    public ResponseEntity<ResponseDto<Collection<ResponseListarPessoasNomePeriodo>>> listarPessoasPorNomePeriodo(
             @RequestParam String nome,
             @RequestParam String dataInicio,
             @RequestParam String dataFim) {
-        //TODO: Fazer uma validação de horas mais robustas
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
         LocalDateTime inicio = LocalDateTime.parse(dataInicio + "T00:00:00", formatter);
         LocalDateTime fim = LocalDateTime.parse(dataFim + "T23:59:59", formatter);
 
-        return ResponseEntity.ok(pessoaService.listarPessoasPorNomePeriodo(nome, inicio, fim));
+        Collection<ResponseListarPessoasNomePeriodo> lista = pessoaService.listarPessoasPorNomePeriodo(nome, inicio, fim);
+        return ResponseEntity.ok(ResponseDto.sucess(lista));
     }
 
     @GetMapping()
-    public ResponseEntity<Collection<ResponseListarPessoasTotalHoraTarefa>> listarPessoas() {
-        return ResponseEntity.ok(pessoaService.listarPessoasComTotalHorasGastas());
+    public ResponseEntity<ResponseDto<Collection<ResponseListarPessoasTotalHoraTarefa>>> listarPessoas() {
+        Collection<ResponseListarPessoasTotalHoraTarefa> lista = pessoaService.listarPessoasComTotalHorasGastas();
+        return ResponseEntity.ok(ResponseDto.sucess(lista));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> cadastrarPessoa(@RequestBody @Valid final RequestCadastrarPessoa requestCadastrarPessoa) {
+    public ResponseEntity<ResponseDto<String>> cadastrarPessoa(@RequestBody @Valid final RequestCadastrarPessoa requestCadastrarPessoa) {
         return Optional.of(requestCadastrarPessoa)
                 .map(pessoaDtoMapper::toModel)
                 .map(pessoaService::cadastrarPessoa)
-                .map(pessoa -> ResponseEntity.status(HttpStatus.CREATED)
-                        .body("Pessoa criada com sucesso! ID: " + pessoa.getId()))
+                .map(pessoaCadastrada -> {
+                    String mensagem = "Pessoa criada com sucesso! ID: " + pessoaCadastrada.getId();
+                    return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.sucess(mensagem));
+                })
                 .orElseThrow();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> atualizarPessoa(
+    public ResponseEntity<ResponseDto<?>> atualizarPessoa(
             @PathVariable final Long id,
             @RequestBody final RequestAtualizarPessoa requestAtualizarPessoa) {
-        return Optional.of(requestAtualizarPessoa)
-                .map(pessoaDtoMapper::toModel)
-                .map(pessoaAtualizada -> {
-                    pessoaService.atualizarPessoa(id, pessoaAtualizada);
-                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-                })
-                .orElseThrow();
+        pessoaService.atualizarPessoa(id, pessoaDtoMapper.toModel(requestAtualizarPessoa));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDto.sucess());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletarPessoa(@PathVariable final Long id){
-        return Optional.ofNullable(id)
-                .map(pessoaId -> {
-                    pessoaService.excluirPessoa(id);
-                    return id;
-                })
-                .map(pessoaId -> ResponseEntity.status(HttpStatus.NO_CONTENT).build())
-                .orElseThrow();
+    public ResponseEntity<ResponseDto<?>> deletarPessoa(@PathVariable final Long id){
+        pessoaService.excluirPessoa(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDto.sucess());
     }
-
 }
--- src/main/java/org/perenity/gerenciamentotarefas/presentation/tarefa/controller/TarefaController.java
+++ src/main/java/org/perenity/gerenciamentotarefas/presentation/tarefa/controller/TarefaController.java
@@ -1,16 +1,15 @@
 package org.perenity.gerenciamentotarefas.presentation.tarefa.controller;
 
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.perenity.gerenciamentotarefas.business.tarefa.service.TarefaService;
+import org.perenity.gerenciamentotarefas.infra.ResponseDto;
 import org.perenity.gerenciamentotarefas.presentation.tarefa.dto.request.RequestAlocarPessoaTarefa;
 import org.perenity.gerenciamentotarefas.presentation.tarefa.dto.request.RequestCadastrarTarefa;
 import org.perenity.gerenciamentotarefas.presentation.tarefa.dto.request.RequestFinalizarTarefa;
 import org.perenity.gerenciamentotarefas.presentation.tarefa.dto.response.ResponseListarTarefasPendentes;
 import org.perenity.gerenciamentotarefas.presentation.tarefa.mapper.TarefaDtoMapper;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,52 +25,43 @@ public class TarefaController {
     private final TarefaService tarefaService;
 
     @GetMapping("/pendentes")
-    public ResponseEntity<Collection<ResponseListarTarefasPendentes>> listarTarefasPendentes(){
-        return ResponseEntity.ok()
-                .body(tarefaService.listarTarefasPendentes()
+    public ResponseEntity<ResponseDto<Collection<ResponseListarTarefasPendentes>>> listarTarefasPendentes() {
+        Collection<ResponseListarTarefasPendentes> lista = tarefaService.listarTarefasPendentes()
                 .stream()
                 .map(tarefaDtoMapper::toResponse)
-                .toList());
+                .toList();
+
+        return ResponseEntity.ok(ResponseDto.sucess(lista));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> cadastrarTarefa(
-            @RequestBody @Valid final RequestCadastrarTarefa requestCadastrarTarefa){
+    public ResponseEntity<ResponseDto<String>> cadastrarTarefa(
+            @RequestBody @Valid final RequestCadastrarTarefa requestCadastrarTarefa) {
         return Optional.of(requestCadastrarTarefa)
                 .map(tarefaDtoMapper::toModel)
                 .map(tarefaService::cadastrarTarefa)
-                .map(tarefa -> ResponseEntity.status(HttpStatus.CREATED)
-                        .body("Tarefa criada com sucesso! ID: " + tarefa.getId()))
-                .orElseThrow();
+                .map(tarefa -> {
+                    String mensagem = "Tarefa criada com sucesso! ID: " + tarefa.getId();
+                    return ResponseEntity.status(HttpStatus.CREATED).body(ResponseDto.sucess(mensagem));
+                })
+                .orElseThrow(); // nunca deve cair aqui pois o corpo é validado
     }
 
     @PutMapping("/alocar/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<?> alocarPessoaTarefa(
+    public ResponseEntity<ResponseDto<?>> alocarPessoaTarefa(
             @PathVariable final Long id,
-            @RequestBody @Valid final RequestAlocarPessoaTarefa requestAlocarPessoaTarefa){
-        return Optional.ofNullable(requestAlocarPessoaTarefa)
-                .map(tarefaDtoMapper::toModel)
-                .map(tarefa -> {
-                    tarefaService.alocarTarefaPessoa(id, tarefa);
-                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-                })
-                .orElseThrow();
+            @RequestBody @Valid final RequestAlocarPessoaTarefa requestAlocarPessoaTarefa) {
+
+        tarefaService.alocarTarefaPessoa(id, tarefaDtoMapper.toModel(requestAlocarPessoaTarefa));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDto.sucess());
     }
 
     @PutMapping("/finalizar/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public ResponseEntity<?> finalizarTarefa(
+    public ResponseEntity<ResponseDto<?>> finalizarTarefa(
             @PathVariable final Long id,
-            @RequestBody @Valid final RequestFinalizarTarefa requestFinalizarTarefa){
-        return Optional.ofNullable(requestFinalizarTarefa)
-                .map(tarefaDtoMapper::toModel)
-                .map(tarefa -> {
-                    tarefaService.finalizarTarefa(id, tarefa);
-                    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-                })
-                .orElseThrow();
-    }
+            @RequestBody @Valid final RequestFinalizarTarefa requestFinalizarTarefa) {
 
+        tarefaService.finalizarTarefa(id, tarefaDtoMapper.toModel(requestFinalizarTarefa));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ResponseDto.sucess());
+    }
 }
